ba80da19ac49f4afe9f2b3dd199aadd7741a65dd
Waqar Mohammad <waqar@waqar.co>
2017-02-21 18:20:31 +0000

    task 001

---
+++ task001.php
@@ -0,0 +1,13 @@
+<?php
+
+// task 001
+
+echo nl2br('I went down to the river,
+I set down on the bank.
+I tried to think but couldn\'t,
+So I jumped in and sank.
+
+I came up once and hollered!
+I came up twice and cried!
+If that water hadn\'t a-been so cold
+I might\'ve sunk and died.');
